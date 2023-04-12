45bd2c4238a62db50b9d15f4e1dbb1b1b92bb155
eva.minaud <eva.minau@gmail.com>
2023-04-12 16:24:20 +0200

    bidouille qui mènent à rien

--- src/Form/TacheRealiseType.php
+++ src/Form/TacheRealiseType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Editeur;
-use App\Tools\ModeleTache;
+use App\Entity\TacheRealise;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,84 +15,20 @@ class TacheRealiseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('editeur1', EntityType::class, [
+            ->add('editeur', EntityType::class, [
+                'label' => 'Utilisateur',
                 'class' => Editeur::class,
-                'label' => false,
                 'placeholder' => '-- Saisir un utilisateur --'
             ])
-            ->add('editeur2', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])/*
-            ->add('editeur3', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur4', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur5', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur6', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur7', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur8', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur9', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur10', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur11', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur12', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])
-            ->add('editeur13', EntityType::class, [
-                'class' => Editeur::class,
-                'label' => false,
-                'placeholder' => '-- Saisir un utilisateur --'
-            ])*/
+
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => ModeleTache::class,
+            'data_class' => TacheRealise::class,
             'allow_extra_fields' => true
         ]);
-    }
-
-    public function getBlockPrefix(): string
-    {
-        return '';
     }
 }
